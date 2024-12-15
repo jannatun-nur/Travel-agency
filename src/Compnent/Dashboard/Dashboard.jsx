1536dd2e44106c3ab17745648034cf2fec5e0549
jannatun-nur <jannatunur03@gmail.com>
2024-12-15 09:04:13 +0600

    navbar 40%

--- src/Compnent/Dashboard/Dashboard.jsx
+++ src/Compnent/Dashboard/Dashboard.jsx
@@ -2,16 +2,16 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 const Dashboard = () => {
     return (
-        <div className="z-50">
+        <div>
             <div className="drawer">
   <input id="my-drawer" type="checkbox" className="drawer-toggle" />
   <div className="drawer-content">
     {/* Page content here */}
-    <label htmlFor="my-drawer" className="btn btn-primary drawer-button"><GiHamburgerMenu /></label>
+    <label htmlFor="my-drawer" className="text-white font-bold lg:text-2xl"><GiHamburgerMenu /></label>
   </div>
   <div className="drawer-side z-50">
     <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+    <ul className="menu  text-base-content min-h-full w-8/12 lg:w-80 p-4 bg-white shadow-2xl shadow-blue-900">
       {/* Sidebar content here */}
       <li><a>Sidebar Item 1</a></li>
       <li><a>Sidebar Item 2</a></li>
--- src/Compnent/Navbar.jsx
+++ src/Compnent/Navbar.jsx
@@ -1,62 +1,57 @@
 import Dashboard from "./Dashboard/Dashboard";
 
-
 const Navbar = () => {
-    return (
-        <div className="navbar bg-white bg-opacity-60">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16" />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
-            </ul>
-          </div>
+  const navlinks = (
+    <>
+      <li>About</li>
+      <li>Package</li>
+      <li>blog</li>
+    </>
+  );
 
-
-          <div>
-            <Dashboard/>
-          </div>
-        </div>
-
-
-
-
-
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>About</li>
-            <li>
-             Package
-            </li>
-            <li>blog</li>
+  return (
+    <div className="navbar fixed z-10 bg-opacity-25 bg-base-10">
+      <div className="navbar-start">
+        <div className="dropdown">
+          {/* <div tabIndex={0} role="button" className="btn btn-ghost hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div> */}
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {navlinks}
           </ul>
         </div>
-       
+
+        <div>
+          <Dashboard  />
+          <p className="block lg:hidden">
+            {navlinks}
+          </p>
+        </div>
       </div>
-    );
+
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-10 text-white lg:text-xl">
+          {navlinks}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
